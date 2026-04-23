d4f6a3b313c08b113d0c44bd74666f73bb4c8eeb
Shanaev-lesha <shanaev007@gmail.com>
2026-04-24 01:00:26 +0400

    стилизовал кнопку на главной странице

--- React_Quiz_Packs_Manager/src/pages/Dashboard.jsx
+++ React_Quiz_Packs_Manager/src/pages/Dashboard.jsx
@@ -1,4 +1,5 @@
 import { useAuthStore } from "../store/auth.store";
+import { Button } from "@mui/material";
 
 export const Dashboard = () => {
   const logout = useAuthStore((s) => s.logout);
@@ -6,7 +7,24 @@ export const Dashboard = () => {
   return (
     <div>
       <h1>Dashboard</h1>
-      <button onClick={logout}>Logout</button>
+      <Button
+        onClick={logout}
+        type="submit"
+        variant="contained"
+        sx={{
+          mt: 1,
+          py: 1.3,
+          borderRadius: 2,
+          fontWeight: "bold",
+          background: "linear-gradient(90deg, #9c27b0, #ba68c8)",
+          boxShadow: "0 0 20px rgba(156,39,176,0.5)",
+          "&:hover": {
+            boxShadow: "0 0 30px rgba(156,39,176,0.8)",
+          },
+        }}
+      >
+        Logout
+      </Button>
     </div>
   );
 };
